d72cecb22ca9b4dc33da9b85e33b55e95e3dc63a
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2022-09-12 12:04:11 +0200

    Exercise 1 lesson 3 added

---
+++ MyT/Lesson 3/CompressionStockingCore/CompressionStocking/CompressionCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CompressionStocking
+{
+    public class CompressionCtrl: ICompressionCtrl
+    {
+        Pump p1 = new Pump();
+        private bool isCompressed;
+       
+        public void Compress()
+        {
+            if (isCompressed == false){ 
+                p1.InitialPump();
+                isCompressed = true;
+            }
+            else
+            {
+                Console.Write("Cannot compress");
+            }
+        }
+
+        public void Decompress()
+        {
+            if (isCompressed == true)
+            {
+                p1.ReversePump();
+                isCompressed = false;
+            }
+            else
+            {
+                Console.Write("Cannot decompress");
+            }
+        }
+
+    }
+}
---
+++ MyT/Lesson 3/CompressionStockingCore/CompressionStocking/Pump.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CompressionStocking
+{
+    public class Pump
+    {
+        public void InitialPump()
+        {
+            System.Console.WriteLine("Pumps for 5 seconds");
+            Thread.Sleep(5000);
+        }
+        public void ReversePump()
+        {
+            System.Console.WriteLine("Pumps Backwards for 2 seconds");
+            Thread.Sleep(2000);
+            // pump run backwards 2 seconds
+            // stop
+            System.Console.WriteLine("Stops");
+        }
+    }
+}
