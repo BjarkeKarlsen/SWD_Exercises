d06309f7df4ad37c29b231e9bb9c9e9218403e44
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2022-09-29 09:29:48 +0200

    uploaded exercise5

---
+++ MyT/Lesson 5/Exercise5/Exercise5/SuperSorter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Exercise5
+{
+    public class SuperSorter
+    {
+        private int[] data_;
+        public SuperSorter(int[] data)
+        {
+ 
+            data_ = data;
+            IntArrayInsertionSort(data_);
+ 
+        }
+
+        public static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        }
+
+        
+        public void print()
+        {
+            foreach (var item in data_)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
+
---
+++ MyT/Lesson 5_2/Exercise5_2/Exercise5/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exercise5;
+using System;
+
+ArrayGenerator array1 = new ArrayGenerator(10,10);
+array1.Generate();
+array1.print();
+
+int[] array2 = array1.GetArray();
+
+SuperSorter sortedArray = new SuperSorter(array2);
+sortedArray.print();
